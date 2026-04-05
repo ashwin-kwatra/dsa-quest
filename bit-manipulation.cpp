11522d46c5e7cd3f3f676dc9240e6a936a1565ea
ashwin-kwatra <ashwin.kwatra.5@gmail.com>
2026-04-05 23:53:53 +0530

    few leet questions

--- bit-manipulation.cpp
+++ bit-manipulation.cpp
@@ -1,6 +1,7 @@
 #include<string.h>;
 #include<iostream>;
 #include<stdio.h>;
+#include<vector>;
 //leet 231 
 class Solution {
 public:
@@ -12,3 +13,62 @@ public:
         
     }
 };
+
+//counting no of set bits
+int countSetBits(int n) {
+    int count = 0;
+    while (n > 0) {
+        count += (n % 2);  
+        n = n / 2;         
+    }
+    return count;
+}
+
+//leet 2220 ( to find mionimum bit flips to turn number a to b)
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int p= start ^ goal ;
+        int ctr=0;
+        while(p){
+            if((p & 1)==1){
+                ctr+=1;
+            }
+            p= p >> 1;
+        }
+        return ctr;
+    }
+};
+
+//leet 136 (unique number)
+class Solution {
+public:
+    int singleNumber(std::vector<int>& nums) {
+        int res = 0;
+        for (int n : nums) {
+            res ^= n;
+        }
+        return res;        
+    }
+};
+
+//leet 78 , generating subsets
+class Solution {
+public:
+    std::vector<std::vector<int>> subsets(std::vector<int>& nums) {
+        int n = nums.size();
+        std::vector<std::vector<int>> b;
+        for(int i = 0; i < (1 << n); i++){
+            std::vector<int> l;
+            for(int j = 0; j < n; j++){
+                if((i & (1 << j)) != 0){
+                    l.push_back(nums[j]);
+                }
+            }
+            b.push_back(l);
+        }
+        return b;
+    }
+};
+
+//
